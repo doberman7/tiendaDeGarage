3a510ae6f111c5dc6c4981f36980e59e966b824a
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-01 09:40:00 -0600

    1st comit

---
+++ routes/userRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const {
+  indexView,
+  signupView,
+  loginView,
+  profilePicture,
+  profileView,
+  loginProcess,
+  signupProcessUser,
+  logout,
+  editProfile,
+  deleteProfile,
+  currentUser,
+} = require('../controllers/User.controllers');
+const uploadPicture = require('../config/cloudinary');
+
+/* GET home page */
+// router.get('/', indexView);
+// router.get('/signup', signupView);
+router.post('/signup', signupProcessUser);
+
+// router.get('/login', loginView)
+router.post('/login', loginProcess);
+// router.get('/profile', profileView)
+router.post('/profile-picture', uploadPicture.single('image'), profilePicture);
+router.get('/logout', logout);
+router.post('/editProfile', editProfile);
+router.get('/deleteProfile', deleteProfile);
+router.get('/current-user', currentUser);
+
+module.exports = router;
