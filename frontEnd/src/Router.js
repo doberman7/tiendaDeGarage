7644936b831bafa3c5b8d0ae8856cf5a4011c164
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-01 09:40:00 -0600

    1st comit

---
+++ frontEnd/src/Router.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import LayoutApp from './components/LayoutApp';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import ButtonsHome from './pages/ButtonsHome';
+import './Router.css';
+import { Button } from 'antd';
+import { Divider } from 'antd';
+
+// const Login = () => <h1>Login</h1>
+// const Signup = () => <h1>Signup</h1>
+// const Profile = () => <h1>Profile</h1>
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <LayoutApp>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+
+        <ButtonsHome />
+      </LayoutApp>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
