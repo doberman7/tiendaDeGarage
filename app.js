971764552ca8e6875c98f36df583fa4dc5fb3a22
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-03 12:01:26 -0600

    passopor devuelve user

--- app.js
+++ app.js
@@ -40,8 +40,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(passport.initialize());
-app.use(passport.session());
+
 app.use(flash());
 app.use(
   cors({
