463c41d091c25878c1968509b3edaa3fc6868366
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-03 19:01:28 -0600

    new pic image

--- models/User.Model.js
+++ models/User.Model.js
@@ -1,24 +1,26 @@
-const {
-  Schema,
-  model
-} = require('mongoose')
+const { Schema, model } = require('mongoose');
 
-
-const userSchema = new Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: String,
+    name: {
+      type: String,
+      trim: true,
+    },
+    picture: {
+      type: String,
+      default:
+        'https://image.shutterstock.com/image-vector/user-avatar-icon-sign-profile-260nw-1145752283.jpg',
+    },
   },
-  password: String,
-  name: {
-    type: String,
-    trim: true,
-  },
-  picture: { type: String, default: 'https://memegenerator.net/img/instances/67438689/oh-great-im-a-profile-photo.jpg' },
+  {
+    timestamps: true,
+  }
+);
 
-}, {
-  timestamps: true
-})
-
-module.exports = model("User", userSchema)
+module.exports = model('User', userSchema);
