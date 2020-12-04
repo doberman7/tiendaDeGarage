4ad56bc66d630fbd9a900efdafaf4a8cb34272ec
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-03 19:01:28 -0600

    new pic image

--- frontEnd/src/pages/Profile.js
+++ frontEnd/src/pages/Profile.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Typography, Row, Col } from 'antd';
+import { Typography, Row, Col, Avatar, Image } from 'antd';
 import { useContextInfo } from '../hooks/context';
 import { Redirect } from 'react-router-dom';
-//import UploadProfPic from "./UploadProfPic"
+import { UserOutlined } from '@ant-design/icons';
 
 const Profile = ({ history }) => {
   const { user } = useContextInfo();
-
+  console.log('el USER:', user);
   //
   return user ? (
     <Row>
       <Col xs={24} sm={24} md={12}>
         <Typography.Title level={3}>
+          <Avatar src={user.picture} />
+          <br></br>
           Profile: {user.email}
           <br></br>
           ID: {user._id}
