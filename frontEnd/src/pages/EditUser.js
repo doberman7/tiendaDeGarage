fe715f77af54bb0725326cd05544a458a99b7ace
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-06 11:43:18 -0600

    using antD form

--- frontEnd/src/pages/EditUser.js
+++ frontEnd/src/pages/EditUser.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
-import { Row, Col, Form, Input, Button, Typography, Divider } from 'antd';
+import {
+  Row,
+  Col,
+  Form,
+  Input,
+  Button,
+  Typography,
+  Divider,
+  Upload,
+} from 'antd';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import axios from 'axios';
 import { editProfileFn } from '../services/auth';
 import { useContextInfo } from '../hooks/context';
 import UploadProfilePic from '../components/UploadProfPic';
+
+const cloudinaryAPI =
+  'https://api.cloudinary.com/v1_1/lab-file-upload2/image/upload';
 
 const { Title } = Typography;
 
@@ -11,6 +25,8 @@ const EditUser = ({ history }) => {
   const { setUserUpdtade } = useContextInfo();
   const [error, setError] = useState(null);
   const { user } = useContextInfo();
+  const [img, setImg] = useState(null);
+  const [loading, setLoading] = useState(null);
 
   async function handleEditUser(userInput) {
     try {
@@ -25,6 +41,27 @@ const EditUser = ({ history }) => {
     } finally {
     }
   }
+  async function handleUploadFile(file) {
+    // console.log(info);
+    setLoading(true);
+    const data = new FormData();
+
+    data.append('file', file);
+    data.append('upload_preset', 'huntfake');
+
+    const {
+      data: { secure_url },
+    } = await axios.post(cloudinaryAPI, data);
+
+    setImg(secure_url);
+    setLoading(false);
+  }
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
   // console.log(user);
   return user ? (
     <Row>
@@ -41,13 +78,20 @@ const EditUser = ({ history }) => {
           <Form.Item name="password" label="Password:">
             <Input.Password />
           </Form.Item>
+          <Form.Item name="image" label="Image:">
+            <Upload
+              name="image"
+              showUploadList={false}
+              beforeUpload={handleUploadFile}
+            >
+              {img ? <img src={img} style={{ width: '100%' }} /> : uploadButton}
+            </Upload>
+          </Form.Item>
+
           <Button type="primary" htmlType="submit" block>
             EditUser
           </Button>
-
-          <hr />
         </Form>
-        <UploadProfilePic></UploadProfilePic>
       </Col>
     </Row>
   ) : (
