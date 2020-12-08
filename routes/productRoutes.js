0ca8cd12e4f78e1fdee031d63ec4992f57b5fa36
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-08 15:43:09 -0600

    post->get

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -10,6 +10,6 @@ const {
 router.post('/create', createProcessProduct);
 router.put('/edit/:id', editProduct);
 router.delete('/delete/:id', deleteProduct);
-router.post('/get-user-products', getUserProducts);
+router.get('/get-user-products', getUserProducts);
 
 module.exports = router;
