0915fb645155146af0e2150b0393f1471a91200e
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-08 12:11:45 -0600

    card instead of carrousel

--- frontEnd/src/pages/ViewProducts.js
+++ frontEnd/src/pages/ViewProducts.js
@@ -1,35 +1,26 @@
 import React from 'react';
 import { useContextInfo } from '../hooks/context';
 import { Redirect } from 'react-router-dom';
-import { Carousel } from 'antd';
-function onChange(a, b, c) {
-  console.log(a, b, c);
-}
-const contentStyle = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
+import { Card } from 'antd';
+
+const { Meta } = Card;
+
 const Profile = ({ history }) => {
   const { user } = useContextInfo();
 
   return user ? (
-    <Carousel afterChange={onChange}>
-      <div>
-        <h3 style={contentStyle}>1</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>2</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>3</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>4</h3>
-      </div>
-    </Carousel>
+    <Card
+      hoverable
+      style={{ width: 240 }}
+      cover={
+        <img
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      }
+    >
+      <Meta title="Europe Street beat" description="www.instagram.com" />
+    </Card>
   ) : (
     <Redirect to="/" />
   );
