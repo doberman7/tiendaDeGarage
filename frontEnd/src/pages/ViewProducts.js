c9a1919076e9161113314d4c0088a2bcd5efba48
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-08 18:30:07 -0600

    titles on froont

--- frontEnd/src/pages/ViewProducts.js
+++ frontEnd/src/pages/ViewProducts.js
@@ -23,22 +23,21 @@ const Profile = ({ history }) => {
   return user ? (
     <div>
       {userProducts?.map((product) => (
-        <p>product</p>
+        <Card
+          hoverable="hoverable"
+          style={{
+            width: 240,
+          }}
+          cover={
+            <img
+              alt="example"
+              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            />
+          }
+        >
+          <Meta title={product.name} description="www.instagram.com" />
+        </Card>
       ))}
-      <Card
-        hoverable="hoverable"
-        style={{
-          width: 240,
-        }}
-        cover={
-          <img
-            alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-          />
-        }
-      >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
-      </Card>
     </div>
   ) : (
     <Redirect to="/" />
