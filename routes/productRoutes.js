187b77673f59f1f8b962b8ca25d863c6dd31dee5
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-08 22:21:58 -0600

    goes to product detaiil

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,11 +5,15 @@ const {
   editProduct,
   deleteProduct,
   getUserProducts,
+  getProductDetails,
 } = require('../controllers/Product.Controllers');
 
 router.post('/create', createProcessProduct);
 router.put('/edit/:id', editProduct);
 router.delete('/delete/:id', deleteProduct);
 router.get('/get-user-products', getUserProducts);
+
+router.get('/get-product-details/:productId', getProductDetails);
+router.get('/product/edit-product', editProduct);
 
 module.exports = router;
