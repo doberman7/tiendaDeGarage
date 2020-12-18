dc45e49a23b7dab9fa0dd9b2506ce76e0a40ebb0
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-18 12:38:43 -0600

    DONE no home once logiin

--- frontEnd/src/components/LayoutApp.js
+++ frontEnd/src/components/LayoutApp.js
@@ -18,11 +18,11 @@ const LayoutApp = ({ children }) => {
       <Header>
         <div className="logo" />
         <Menu theme="dark" mode="horizontal">
-          <Menu.Item key="1">
-            <Link to="/">Home</Link>
-          </Menu.Item>
           {!user ? (
             <>
+              <Menu.Item key="1">
+                <Link to="/">Home</Link>
+              </Menu.Item>
               <Menu.Item key="2">
                 <Link to="/signup">Signup</Link>
               </Menu.Item>
