8d2249d391d778b6a3f882bb48585393c82adb44
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-19 17:47:32 -0600

    add images and explanation of the app

--- frontEnd/src/pages/Welcome.js
+++ frontEnd/src/pages/Welcome.js
@@ -6,27 +6,32 @@ import { Divider } from 'antd';
 import { useContextInfo } from '../hooks/context';
 import { Image, Button } from 'antd';
 function ImageDemo() {
-  return (
-    <Image
-      width={400}
-      src="https://images.unsplash.com/photo-1533745848184-3db07256e163?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80"
-    />
-  );
+  return <Image width={200} src="open.jpg" />;
 }
 const Router = () => {
   const { user } = useContextInfo();
 
   return user ? (
-    <div className="site-button-ghost-wrapper">
-      <ImageDemo />
-      <Divider />
-      <p>welcome {user.email}</p>
-
-      <Button type="primary" ghost>
-        <Link to="/profile">Got it</Link>
-      </Button>
-      <Divider />
-    </div>
+    <>
+      <div className="site-button-ghost-wrapper">
+        <h1>W E L C O M E </h1>
+        <Divider />
+        <p>You have created a user with email: {user.email}</p>
+        <ImageDemo />
+        <p>In this app you can create Wishes and Items</p>
+        <ul>
+          <li>
+            <b>Wishes:</b> things you d wanna buy
+          </li>
+          <li>
+            <b>Items:</b> the stuff you have and want to sell
+          </li>
+        </ul>
+        <Button type="primary" ghost>
+          <Link to="/profile">Got it</Link>
+        </Button>
+      </div>
+    </>
   ) : (
     <Redirect to="/" />
   );
