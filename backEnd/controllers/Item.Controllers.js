5e24e35ee09b9d61b75280b72ca327dc8cfe7332
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-20 13:20:52 -0600

    responde to back

--- backEnd/controllers/Item.Controllers.js
+++ backEnd/controllers/Item.Controllers.js
@@ -1,31 +1,28 @@
-const Item = require("../models/Item.Model")
-const User = require("../models/User.Model")
+const Item = require('../models/Item.Model');
+const User = require('../models/User.Model');
 
-exports.getUserItems = async(req,res) => {
-  const { user:{id}} = req
-  const items = await User.findById(id).populate("items")
-console.log(id)
-  res.status(200).json(items)
-}
+exports.getUserItems = async (req, res) => {
+  const {
+    user: { id },
+  } = req;
+  const items = await User.findById(id).populate('items');
+  console.log(id);
+  res.status(200).json(items);
+};
 
-exports.getItemDetails = async (req,res) => {
-  const {itemId} = req.params
-  const item = await Item.findById(itemId)
+exports.getItemDetails = async (req, res) => {
+  const { itemId } = req.params;
+  const item = await Item.findById(itemId);
 
-  res.status(200).json(item)
-}
+  res.status(200).json(item);
+};
 
 exports.createItem = async (req, res) => {
+  const { title, description, image, price, category, status } = req.body;
   const {
-    title,
-    description,
-    image,
-    price,
-    category,
-    status
-  }= req.body
-  const {user: {id}}  = req
-  console.log(id,title)
+    user: { id },
+  } = req;
+
   const newItem = await Item.create({
     title,
     description,
@@ -33,39 +30,34 @@ exports.createItem = async (req, res) => {
     price,
     category,
     status,
-  })
+  });
 
-  await User.findByIdAndUpdate(id, { $push: { items: newItem._id}})
+  await User.findByIdAndUpdate(id, { $push: { items: newItem._id } });
 
-  res.status(201).json(newItem)
+  res.status(201).json(newItem);
+};
 
-}
+exports.editItem = async (req, res) => {
+  const { itemId } = req.params;
+  const { title, description, image, price, category, status } = req.body;
 
-exports.editItem = async (req,res) => {
-  const {itemId} = req.params
-  const {
-    title,
-    description,
-    image,
-    price,
-    category,
-    status
-  } = req.body
+  const updatedItem = await Item.findByIdAndUpdate(
+    itemId,
+    {
+      title,
+      description,
+      image,
+      price,
+      category,
+      status,
+    },
+    { new: true }
+  );
+  res.status(200).json(updatedItem);
+};
 
-const updatedItem = await Item.findByIdAndUpdate(
-  itemId,{
-    title,
-    description,
-    image,
-    price,
-    category,
-    status
-  }, {new: true})
-res.status(200).json(updatedItem)
-}
-
-exports.deleteItem = async (req,res) => {
-  const {itemId} = req.params
-  await Item.findByIdAndDelete(itemId)
-  res.status(200).json({message: "item deleted"})
-}
+exports.deleteItem = async (req, res) => {
+  const { itemId } = req.params;
+  await Item.findByIdAndDelete(itemId);
+  res.status(200).json({ message: 'item deleted' });
+};
--- backEnd/models/Product.Model.js
+++ backEnd/models/Product.Model.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose');
 const productSchema = new Schema(
   {
     idUser: Schema.Types.ObjectId,
+    category: [String],
     userCreator: { type: Schema.Types.ObjectId, ref: 'User' },
     name: {
       type: String,
