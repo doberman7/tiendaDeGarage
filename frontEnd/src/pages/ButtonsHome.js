f6499eb4a0854b2f48c42a615ddd1c4b4b52bcd3
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-20 10:58:47 -0600

    carrousel on home

--- frontEnd/src/pages/ButtonsHome.js
+++ frontEnd/src/pages/ButtonsHome.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 
 import '../Router.css';
-import { Button } from 'antd';
-import { Divider } from 'antd';
+import { Button, Carousel, Divider, Image } from 'antd';
 import { useContextInfo } from '../hooks/context';
-import { Image } from 'antd';
+const contentStyle = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+
 function ImageDemo() {
   return (
     <Image
@@ -18,26 +24,40 @@ const Router = () => {
   //es importa recordar que user el JSON de la respuesta del back end, no necesariamente un usuario
   const { user } = useContextInfo();
   return !user ? (
-    <div className="site-button-ghost-wrapper">
-      <h1>Tienda de Garage</h1>
-
-      <ImageDemo />
-      <Divider />
-      <p>
-        La experiencia más amena en compras e intercambios de sillas, mesas,
-        trastos, aparatos electronicos, jarras, vasos, plato, cucharas,
-        tenedores reglas, lápizes, mochilas y demás chunches que puedas
-        encontrar en tu garage sólo aquí con su Tienda Garage
-      </p>
-      <Button type="primary" ghost>
-        <Link to="/Signup">Sign up</Link>
-      </Button>
-      <Divider />
-      <Button type="primary" ghost>
-        <Link to="/Login">Login</Link>
-      </Button>
-      <Divider />
-    </div>
+    <>
+      <div className="site-button-ghost-wrapper">
+        <h1>Tienda de Garage</h1>
+        <Carousel autoplay>
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
+        <Divider />
+        <p>
+          La experiencia más amena en compras e intercambios de sillas, mesas,
+          trastos, aparatos electronicos, jarras, vasos, plato, cucharas,
+          tenedores reglas, lápizes, mochilas y demás chunches que puedas
+          encontrar en tu garage sólo aquí con su Tienda Garage
+        </p>
+        <Button type="primary" ghost>
+          <Link to="/Signup">Sign up</Link>
+        </Button>
+        <Divider />
+        <Button type="primary" ghost>
+          <Link to="/Login">Login</Link>
+        </Button>
+        <Divider />
+      </div>
+    </>
   ) : (
     <Redirect to="/profile" />
   );
