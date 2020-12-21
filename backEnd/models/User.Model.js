666f736994821d369d2f3b5c275e8eeda96d0811
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-21 14:58:26 -0600

    if user have products or items goes to /profile

--- backEnd/models/User.Model.js
+++ backEnd/models/User.Model.js
@@ -2,35 +2,37 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
-    googleId: String,
+    name: {
+      type: String,
+      trim: true,
+      default: 'New User',
+    },
     email: {
       type: String,
       unique: true,
       required: true,
     },
-    password: String,
+
+    picture: {
+      type: String,
+      default:
+        'https://image.shutterstock.com/image-vector/user-avatar-icon-sign-profile-260nw-1145752283.jpg',
+    },
     items: [
       {
         type: Schema.Types.ObjectId,
         ref: 'Item',
       },
     ],
-    name: {
-      type: String,
-      trim: true,
-      default: 'New User',
-    },
-    picture: {
-      type: String,
-      default:
-        'https://image.shutterstock.com/image-vector/user-avatar-icon-sign-profile-260nw-1145752283.jpg',
-    },
+
     products: [
       {
         type: Schema.Types.ObjectId,
         ref: 'Product',
       },
     ],
+    googleId: String,
+    password: String,
   },
   {
     timestamps: true,
