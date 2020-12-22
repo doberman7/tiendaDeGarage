24bf9f8f5a9bca2c48a1fb8d6c9322bc594fea7c
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-12-21 19:49:36 -0600

    back gets all items

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const {
-    createItem,
-    deleteItem,
-    getItemDetails,
-    getUserItems,
-    editItem,
-    addItemImage
-} = require ("../controllers/Item.Controllers")
-const { isAuth, catchErrs}= require("../middlewares")
+  createItem,
+  deleteItem,
+  getItemDetails,
+  getUserItems,
+  editItem,
+  addItemImage,
+  allitems,
+} = require('../controllers/Item.Controllers');
+const { isAuth, catchErrs } = require('../middlewares');
 
-
-router.get("/get-user-items",catchErrs(getUserItems))
-router.get("/get-item-details/:itemId",catchErrs(getItemDetails))
-router.post("/create",catchErrs(createItem))
-router.put("/edit-item/:itemId",catchErrs(editItem))
-router.delete("/delete-item/:itemId",catchErrs(deleteItem))
-
+router.get('/get-user-items', catchErrs(getUserItems));
+router.get('/get-item-details/:itemId', catchErrs(getItemDetails));
+router.post('/create', catchErrs(createItem));
+router.put('/edit-item/:itemId', catchErrs(editItem));
+router.delete('/delete-item/:itemId', catchErrs(deleteItem));
+router.get('/all', allitems);
 module.exports = router;
