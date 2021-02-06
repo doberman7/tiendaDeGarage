0de9db8cf35d4aea045dfb1d9354d0be85a3ea4e
doberman7 <ivanrubioangeles@gmail.com>
2021-02-05 21:55:11 -0600

    new seed

--- backEnd/seeder/seedProducts.js
+++ backEnd/seeder/seedProducts.js
@@ -2,10 +2,12 @@ const Product = require('../models/Product.Model');
 const User = require('../models/User.Model');
 const mongoose = require('mongoose');
 const path = require('path');
+const axios = require('axios');
 
 //create your array. i inserted only 1 object here
 async function seed() {
   try {
+    let times = 20;
     //a random number no biger that 30 to add to names, mail and iterator
     let n = Math.floor(Math.random() * (30 - 1) + 1);
     //create user
@@ -17,22 +19,23 @@ async function seed() {
         'https://lh3.googleusercontent.com/proxy/dmHyFpMzk8e5gcIAIPR1Yk7Tobz1fBSlkL49CVcd9oWbd6p7k9AtzHtQ9U1FJNyx-8iyiWpSiy7ZPSRI3KL_dC5o3hh9mG28wxyPJXQbq6bq836IIt11h8zfzDWJPOEZ5KF8',
     });
 
+    const data = await axios.get(`https://fakestoreapi.com/products/`);
     do {
+      let { price } = data;
       const p = await Product.create({
         idUser: user.id,
         name: 'producto' + n,
         description: './package.jsondescription del producto 1',
         image:
           'https://www.pepsico.cl/images/librariesprovider40/default-album/vb3_montaje-productos-pepsico2e33c3aeef576d378be6ff5000e253d8.jpg?sfvrsn=1',
-        price: n,
+        price: data.price,
         category: 'other',
-      });
+      }).then(console.log(data));
 
       await User.findByIdAndUpdate(user.id, { $push: { products: p._id } });
       await Product.find({ idUser: user.id }).populate('userCreator');
-      n--;
-    } while (n > 0);
-
+      times--;
+    } while (times > 0);
     await mongoose.disconnect();
     console.log('done');
   } catch (error) {
