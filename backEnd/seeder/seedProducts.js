57d966adfb6a791cc1376d08b159231370158ff7
doberman7 <ivanrubioangeles@gmail.com>
2021-02-05 22:23:18 -0600

    seed from fakestoreapi

--- backEnd/seeder/seedProducts.js
+++ backEnd/seeder/seedProducts.js
@@ -6,10 +6,12 @@ const axios = require('axios');
 
 //create your array. i inserted only 1 object here
 async function seed() {
+  let times = 19;
   try {
-    let times = 20;
     //a random number no biger that 30 to add to names, mail and iterator
-    let n = Math.floor(Math.random() * (30 - 1) + 1);
+    // let n = Math.floor(Math.random() * (30 - 1) + 1);
+    let n = Math.floor(Math.random());
+    // let n = 4;
     //create user
     const user = await User.create({
       email: 'seed' + n + '@mail.com',
@@ -18,19 +20,21 @@ async function seed() {
       picture:
         'https://lh3.googleusercontent.com/proxy/dmHyFpMzk8e5gcIAIPR1Yk7Tobz1fBSlkL49CVcd9oWbd6p7k9AtzHtQ9U1FJNyx-8iyiWpSiy7ZPSRI3KL_dC5o3hh9mG28wxyPJXQbq6bq836IIt11h8zfzDWJPOEZ5KF8',
     });
+    //este data es un ary
+    const { data } = await axios.get(`https://fakestoreapi.com/products/`);
 
-    const data = await axios.get(`https://fakestoreapi.com/products/`);
     do {
-      let { price } = data;
+      // data.data.map((product) => console.log(product));
+
       const p = await Product.create({
         idUser: user.id,
-        name: 'producto' + n,
-        description: './package.jsondescription del producto 1',
-        image:
-          'https://www.pepsico.cl/images/librariesprovider40/default-album/vb3_montaje-productos-pepsico2e33c3aeef576d378be6ff5000e253d8.jpg?sfvrsn=1',
-        price: data.price,
+        name: data[times].title,
+        description: data[times].description,
+        image: data[times].image,
+        price: data[times].price,
         category: 'other',
-      }).then(console.log(data));
+      });
+      // .then(console.log(data));
 
       await User.findByIdAndUpdate(user.id, { $push: { products: p._id } });
       await Product.find({ idUser: user.id }).populate('userCreator');
@@ -40,9 +44,9 @@ async function seed() {
     console.log('done');
   } catch (error) {
     console.log(error);
-    console.log(
-      'if MongoError: E11000 duplicate key error collection: mail, runs the seed again'
-    );
+    // console.log(
+    //   'if MongoError: E11000 duplicate key error collection: mail, runs the seed again'
+    // );
   }
 }
 
