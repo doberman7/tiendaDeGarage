6318895969001e96073221e33b8f9505915d372c
doberman7 <ivanrubioangeles@gmail.com>
2021-04-13 18:16:43 -0500

    icons

--- frontEnd/src/components/Cart.js
+++ frontEnd/src/components/Cart.js
@@ -1,4 +1,10 @@
 import { useCart } from 'react-use-cart';
+import { Button } from 'antd';
+import {
+  MinusCircleTwoTone,
+  PlusCircleTwoTone,
+  DeleteTwoTone,
+} from '@ant-design/icons';
 const Cart = () => {
   const {
     isEmpty,
@@ -17,18 +23,20 @@ const Cart = () => {
       <ul>
         {items.map((item) => (
           <li key={item.id}>
-            {item.quantity} x {item.name} &mdash;
-            <button
+            {item.quantity} x {item.name}
+            <Button
               onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
             >
-              -
-            </button>
-            <button
+              <MinusCircleTwoTone />
+            </Button>
+            <Button
               onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
             >
-              +
-            </button>
-            <button onClick={() => removeItem(item.id)}>&times;</button>
+              <PlusCircleTwoTone />
+            </Button>
+            <Button onClick={() => removeItem(item.id)}>
+              <DeleteTwoTone />
+            </Button>
           </li>
         ))}
       </ul>
--- frontEnd/src/pages/Products/Animate/Example.js
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { motion } from 'framer-motion';
-
-export const Example = () => <motion.div animate={{ scale: 2 }} />;
